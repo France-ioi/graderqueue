23d8a7302dd2a0b112271dfec93b6e42637fe2d5
Michel Blockelet <blockelet@crans.org>
2015-07-22 19:46:59 +0200

    SQL schema slight updates
    Use UTF-8, remove AUTO_INCREMENT default values

--- schema.sql
+++ schema.sql
@@ -13,16 +13,17 @@ CREATE TABLE IF NOT EXISTS `done` (
   `done_time` datetime NOT NULL,
   `resultdata` longtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL,
   `log_type` varchar(255) NOT NULL,
   `task_id` int(11) NOT NULL,
   `server_id` int(11) NOT NULL,
   `message` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=253 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `platforms` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -32,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `platforms` (
   `restrict_paths` text NOT NULL,
   `force_tag` int(11) NOT NULL DEFAULT '-1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `queue` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -49,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `queue` (
   `tags` text NOT NULL,
   `taskdata` longtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=113 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `servers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -62,36 +63,36 @@ CREATE TABLE IF NOT EXISTS `servers` (
   `simult_tasks` int(11) NOT NULL DEFAULT '1',
   `last_poll` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `server_types` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `tags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `task_types` (
   `taskid` int(11) NOT NULL,
   `typeid` int(11) NOT NULL,
   KEY `taskid` (`taskid`),
   KEY `typeid` (`typeid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `tokens` (
   `token` varchar(32) NOT NULL,
   `expires` datetime NOT NULL,
   PRIMARY KEY (`token`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `type_tags` (
   `typeid` int(11) NOT NULL,
   `tagid` int(11) NOT NULL,
   KEY `typeid` (`typeid`),
   KEY `tagid` (`tagid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
