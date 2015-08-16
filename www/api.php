1127eaa1feafb0e4837162ebfaa5d0cda297c319
Michel Blockelet <blockelet@crans.org>
2015-08-16 13:42:10 +0200

    Move some code into functions

--- www/api.php
+++ www/api.php
@@ -38,7 +38,7 @@ if(!isset($_POST['request'])) {
       die(jsonerror(2, "taskdata missing from request."));
     }
     try {
-        $taskdata = json_decode($_POST['taskdata']);
+        $evaljson = json_decode($_POST['taskdata']);
     } catch(Exception $e) {
         die(jsonerror(2, "Error while decoding task JSON : " . $e->getMessage()));
     }
@@ -130,41 +130,21 @@ if(!isset($_POST['request'])) {
   }
 
   # Convert tags to list of server types which can execute the task
-  # We make slow requests to have meaningful error messages
-  $tagids = array();
+  if(isset($_POST['tags'])) {
+    $tagids = tags_to_tagids($_POST['tags']);
+  } else {
+    $tagids = array();
+  }
 
+  # Tasks sent by this platform have a tag automatically added    
   if($platdata['force_tag'] != -1) {
     $tagids[] = $platdata['force_tag'];
   }
 
-  if(isset($_POST['tags']) and $_POST['tags'] != '')
-  {
-    $tags = explode(',', $_POST['tags']);
-
-    # Fetch each tag
-    foreach($tags as $t) {
-      $stmt = $db->prepare("SELECT id FROM tags WHERE name = :name;");
-      $stmt->execute(array(':name' => $t));
-      $tagq = $stmt->fetch();
-      if($tagq) {
-        $tagids[] = $tagq[0];
-      } else {
-        die(jsonerror(2, "Tag `" . $t . "` unrecognized."));
-      }
-    }
-  }
-
-  if(count($tagids) > 0) {
-    # Fetch all server types which can execute with these tags
-    $typeids = array();
-    # Data is safe, and a prepared statement would be too complicated
-    $typeq = $db->query("SELECT typeid, COUNT(*) AS nb FROM type_tags WHERE tagid IN (" . implode(',', $tagids) . ") GROUP BY typeid HAVING nb=" . count($tagids) . ";");
-    while($row = $typeq->fetch()) {
-      $typeids[] = $row['typeid'];
-    }
-    if(count($typeids) == 0) {
-      die(jsonerror(2, "No server type can execute tasks with tags " . $_POST['tags'] . "."));
-    }
+  # Fetch all server types which can execute with these tags
+  $typeids = tagids_to_typeids($tagids);
+  if(count($typeids) == 0 && count($tagids) > 0) {
+    die(jsonerror(2, "No server type can execute tasks with tags " . $_POST['tags'] . "."));
   }
 
   # Add path restrictions if needed
@@ -181,7 +161,7 @@ if(!isset($_POST['request'])) {
   $stmt->execute(array(':name' => $taskname, ':priority' => $priority, ':recfrom' => $received_from, ':tags' => $_POST['tags'], ':taskdata' => $jsondata));
 
   $taskid = $db->lastInsertId();
-  if(isset($typeids)) {
+  if(count($typeids) > 0) {
     # Only some server types can execute it
     $db->query("INSERT INTO `task_types` (taskid, typeid) VALUES (" . $taskid . "," . implode("), (" . $taskid . ",", $typeids) . ");");
   } else {
@@ -195,30 +175,7 @@ if(!isset($_POST['request'])) {
   flush();
 
   # Wake up a server if needed
-  $query = "
-    SELECT servers.*,
-      COUNT(queue.id) AS nbtasks
-    FROM `servers`
-    LEFT JOIN queue ON queue.sent_to=servers.id";
-  if(isset($typeids)) {
-    $query .= " WHERE type IN (" . implode(',', $typeids) . ")";
-  }
-  $query .= " GROUP BY servers.id
-    ORDER BY nbtasks DESC, last_poll_time DESC;";
-  $res = $db->query($query);
-  while($row = $res->fetch()) {
-    if(!($row['nbtasks'] < $row['max_concurrent_tasks'] or time()-strtotime($row['last_poll_time'] < 60)))
-    {
-      # Need to wake this server up
-      if(($fs = fsockopen($row['wakeup_url'])) !== FALSE)
-      {
-        fwrite($fs, ' ');
-        fclose($fs);
-        die();
-      }
-      # If failed we'll try the next server
-    }
-  }
+  wake_up_server($typeids);
 
 } elseif($_POST['request'] == "gettask") {
   # Read task information
